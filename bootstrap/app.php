f003e4d429a015cfb4d2bf1b9325070aa9fab83d
Dalia-Elsaeed <daliaallafaf000@gmail.com>
2025-11-20 00:06:36 +0200

    Exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,36 +3,45 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Event;
-use Crm\Customers\Events\CustomerCreation;
-use Crm\Customers\Listeners\NotifySalesOnCustomerCreation;
-// use Crm\Customers\Listeners\SendWelcomeEmail;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use function Illuminate\Log\log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(\App\Http\Middleware\RequestId::class);
-
-        $middleware->append(EnsureFrontendRequestsAreStateful::class);
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->booted(function (): void {
-        Event::listen(
-            CustomerCreation::class,
-            NotifySalesOnCustomerCreation::class,
-        );
-
-        Event::listen(
-            CustomerCreation::class,
-            // SendWelcomeEmail::class,
-        );
+    ->withExceptions(function (Exceptions $exceptions) {
+        // Handle authentication exceptions for API routes
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => "error",
+                    'errors' => [
+                        'generic' => 'Not authenticated'
+                    ]
+                ], JsonResponse::HTTP_UNAUTHORIZED);
+            }
+        });
+        $exceptions->render(function (Throwable $e, Request $request){
+            Log::error($e);
+if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => "error",
+                    'errors' => [
+                        'generic' => 'UNkNOWN ERROR'
+                    ]
+                ], JsonResponse::HTTP_UNAUTHORIZED);
+            }
+        });
     })
     ->create();
